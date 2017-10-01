c83853a785d494a0651a54c19b36e443032da2f8
Tu Nguyen <tounh17@tarleton.edu>
2017-09-30 22:47:05 -0500

    dot prod

--- mul.cu
+++ mul.cu
@@ -4,31 +4,31 @@
 const int shared_size = 4;
 // Square matrix multiplication with dimention that has power of 8
 
-__global__ void kernel(float *a, float *b, float *u, float *v, float *c, int i, int n){
+__global__ void kernel(float *a, float *b, float *c, int i, int n){
 
-  // const int shared_size = n;
+  // // const int shared_size = n;
   __shared__ float cache[shared_size];
-
-  int idx = threadIdx.x;
+  //
+  int k = threadIdx.x;
   int j = blockIdx.x;
-  for(int k = 0, k < n; k++){
-
-  }
-
-
-  if(idx < n){
-    cache[idx] = a[idx] * b[idx];
+  //
+  int a_idx = i*n+k;
+  int b_idx = j+k*n;
+  //
+  if(a_idx < n*n){
+    cache[k] = a[a_idx] * b[b_idx];
   }
   __syncthreads();
-
-  // blockDim has to be a power of 2
+  //
+  // // blockDim has to be a power of 2
   int iter = blockDim.x/2;
   while( iter!= 0){
-    cache[idx] += cache[idx + iter];
+    cache[k] += cache[k + iter];
     __syncthreads();
     iter/=2;
   }
 
+  // c[j+n*i] = a[i];
   c[j+n*i] = cache[0];
 }
 
@@ -59,46 +59,32 @@ int main(int argc, char** argv){
   c_host = (float*) malloc(matDim*sizeof(float));
 
   // Two temp vector
-  float *u_host, *v_host;
-  u_host = (float*) malloc(n*sizeof(float));
-  v_host = (float*) malloc(n*sizeof(float));
+  // float *u_host, *v_host;
+  // // u_host = (float*) malloc(n*sizeof(float));
+  // v_host = (float*) malloc(n*sizeof(float));
 
-  float *u_dev, *v_dev, *c_dev;
-  cudaMalloc((float**) &u_dev, n*sizeof(float));
-  cudaMalloc((float**) &v_dev, n*sizeof(float));
+  float *a_dev, *b_dev, *c_dev;
+  cudaMalloc((float**) &a_dev, matDim*sizeof(float));
+  cudaMalloc((float**) &b_dev, matDim*sizeof(float));
   cudaMalloc((float**) &c_dev, matDim*sizeof(float));
 
   for(int i = 0; i<matDim; i++){
     a_host[i] = i+1;
-    b_host[i] = 1;
+    b_host[i] = i;
   }
+
+  // print_mat(a_host, n);
+
+  cudaMemcpy(a_dev, a_host, matDim*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(b_dev, b_host, matDim*sizeof(float), cudaMemcpyHostToDevice);
 
   // i row
   // j col
   for(int i = 0; i < n; i++){
-    for(int j = 0; j < n; j++){
-        // v_host[k] = a_host[j + (i+k)*n];
-      // printf("%d - %d ---*\n", i, j);
-      for(int k = 0; k < n; k++){
-        u_host[k] = a_host[i*n + k];
-        v_host[k] = b_host[j + k*n];
-      }
-      // print_vec(u_host, n);
-      // print_vec(v_host, n);
-      // printf("---*\n");
-      cudaMemcpy(u_dev, u_host, n*sizeof(float), cudaMemcpyHostToDevice);
-      cudaMemcpy(v_dev, v_host, n*sizeof(float), cudaMemcpyHostToDevice);
-      kernel<<<1, n>>>(u_dev, v_dev, c_dev, i, j, n);
-    }
+      kernel<<<n, n>>>(a_dev, b_dev, c_dev, i, n);
   }
 
   cudaMemcpy(c_host, c_dev, matDim*sizeof(float), cudaMemcpyDeviceToHost);
-
-  // printf("Hello");
-
-  // for(int i = 0; i<n; i++){
-    // printf("%.3f -- %.3f -- %.3f\n", a_host[i], b_host[i], c_host[i]);
-  // }
 
   printf("----\n");
   print_mat(c_host, n);
@@ -107,7 +93,7 @@ int main(int argc, char** argv){
   free(a_host);
   free(b_host);
   free(c_host);
-  cudaFree(u_dev);
-  cudaFree(v_dev);
+  cudaFree(a_dev);
+  cudaFree(b_dev);
   cudaFree(c_dev);
 }
